d4713e155595e909336ac47f2edb25de3d7d85d6
Mike from the macbook <griffinm@gmail.com>
2010-01-27 20:45:32 +0000

    Irish translation

---
+++ translations/jquery.date_input.ga_IE.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Eanáir", "Feabhra", "Márta", "Aibreán", "Bealtaine", "Meitheamh", "Iúil", "Lúnasa", "Meán Fómhair", "Deireadh Fómhair", "Samhain", "Nollaig"],
+  short_month_names: ["Ean", "Fea", "Már", "Aib", "Bea", "Mei", "Iúil", "Lán", "MF", "DF", "Sam", "Nol"],
+  short_day_names: ["Do", "Lu", "Má", "Cá", "Dá", "Ao", "Sa"]
+});
